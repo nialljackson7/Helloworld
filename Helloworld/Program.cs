23e6830fd12f6c1d788f0bb66cf1cc6878181f1e
niall <niall@LAPTOP-F6EA2FF0>
2021-05-11 13:40:11 +0100

    Initial Commit

---
+++ Helloworld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello World!, " + name);
+        }
+    }
+}
